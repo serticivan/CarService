4dc52e337a5928f00acf5267da354a058f41a938
ivan <ivansertic7@gmail.com>
2020-08-17 15:24:46 +0200

    Added homepage.html for UI List last 10 services on homepage Added HomepageController and last_ten_service_list.html Fixed UI

---
+++ src/main/java/hr/inovatrend/carService/controller/HomepageController.java
@@ -0,0 +1,28 @@
+package hr.inovatrend.carService.controller;
+
+import hr.inovatrend.carService.dao.ServiceRepository;
+import hr.inovatrend.carService.domain.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+
+    private final ServiceRepository serviceRepository;
+
+    public HomepageController(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    @RequestMapping("/homepage")
+    private String getAllUsers(Model model) {
+        Page<Service> findLastTenServices = serviceRepository.findAll(PageRequest.of(0,10, Sort.by(Sort.Order.desc("dateNow"))));
+        model.addAttribute("services", findLastTenServices);
+        return "last_ten_service_list";
+    }
+
+}
